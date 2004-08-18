14bb0c0cf927bec7e6a923b0a2a132892d89a940
No Author <dev-null@apache.org>
2004-08-18 20:46:37 +0000

    This commit was manufactured by cvs2svn to create tag 'DIGESTER_1_6_RC1'.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/digester/tags/DIGESTER_1_6_RC1@132699 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/digester/FactoryCreateRule.java
+++ src/java/org/apache/commons/digester/FactoryCreateRule.java
@@ -1,4 +1,4 @@
-/* $Id: FactoryCreateRule.java,v 1.21 2004/05/10 06:30:06 skitching Exp $
+/* $Id: FactoryCreateRule.java,v 1.21.2.1 2004/07/30 20:11:01 rdonkin Exp $
  *
  * Copyright 2001-2004 The Apache Software Foundation.
  * 
@@ -19,6 +19,8 @@
 package org.apache.commons.digester;
 
 import org.xml.sax.Attributes;
+
+import org.apache.commons.collections.ArrayStack;
 
 
 /**
